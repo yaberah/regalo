08e3c249f291b772bb325fa3141e11e6d4944920
yaberah <yabe.masato.ly@alumni.tsukuba.ac.jp>
2020-08-20 00:35:48 +0900

    Add meta and favicon setting

--- components/Layout.js
+++ components/Layout.js
@@ -13,15 +13,18 @@ const Layout = ({children}) => {
     <div id="wrapper">
       <Head>
         <title>横浜のスポーツ整体サロン | Regalo</title>
+        <link rel="icon" href="../static/favicon.svg" type="image/svg+xml" />
         <script src="https://use.typekit.net/zzl2axt.js"></script>
         <script dangerouslySetInnerHTML={{ __html: 'try{Typekit.load({ async: false });}catch(e){}' }} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name = "description" content = "横浜（白楽駅徒歩7分）にあるスポーツ整体サロン。体の悩みを解決するオーダーメイドのメニューを提供。" />
+        <meta name = "description" content = "横浜（白楽駅徒歩7分）にあるスポーツ整体サロン。体の悩みを解決するオーダーメイドのメニューを提供いたします。" />
+        <meta property="og:url" content="https://regalo-salon.work/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="横浜のスポーツ整体サロン Regalo" />
+        <meta property="og:description" content="横浜（白楽駅徒歩7分）にあるスポーツ整体サロン。体の悩みを解決するオーダーメイドのメニューを提供いたします。" />
+        <meta property="og:site_name" content="Regalo" />
+        <meta property="og:image" content="../static/OGP.png" />
         <meta name="twitter:card" content="summary"/>
-        <meta property="og:site_name" name="og:site_name" content="Regalo" />
-        <meta property="og:description" name="og:description" content="横浜（白楽駅徒歩7分）にあるスポーツ整体サロン。体の悩みを解決するオーダーメイドのメニューを提供。" />
-        <meta property="og:title" name="og:title" content="横浜（白楽駅徒歩7分）にあるスポーツ整体サロン。体の悩みを解決するオーダーメイドのメニューを提供。" />
-        <meta property="og:type" name="og:type" content="article" />
       </Head>
       {Reset}
       {Style}
